17c4054394dced8b5cb1c550674007434aa02efd
Diren Ozturk <od.ozturk@student.avans.nl>
2023-11-13 01:12:01 +0100

    enviroment changed

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //key: 'https://tbksocialcreditscore.onrender.com',
-  key: 'http://localhost:3000',
+  key: 'https://tbksocialcreditscore.onrender.com',
+  //key: 'http://localhost:3000',
 };
--- environment.ts
+++ environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //key: 'https://tbksocialcreditscore.onrender.com',
-  key: 'http://localhost:3000',
+  key: 'https://tbksocialcreditscore.onrender.com',
+  //key: 'http://localhost:3000',
 };
